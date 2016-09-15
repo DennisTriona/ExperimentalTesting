3833838104ae387af25d243b00ca38f13e3580aa
INTRATRIONA\FreSan <Fredrik.Sandstrom@triona.se>
2016-09-15 16:47:39 +0200

    ANGULAR_SOCIAL: U: Prepwork for more Angular and Socialsharing experiments

--- ExperimentalTesting/AngularSocial/SocialMediaShareTesting/SocialMediaSharePlayground/app/app.module.js
+++ ExperimentalTesting/AngularSocial/SocialMediaShareTesting/SocialMediaSharePlayground/app/app.module.js
@@ -1,6 +1,3 @@
 (function () {
-    var app = angular.module("app", [
-        "720kb.socialshare"
-    ]);
 });
 //# sourceMappingURL=app.module.js.map
--- ExperimentalTesting/AngularSocial/SocialMediaShareTesting/SocialMediaSharePlayground/app/app.module.ts
+++ ExperimentalTesting/AngularSocial/SocialMediaShareTesting/SocialMediaSharePlayground/app/app.module.ts
@@ -1,5 +1,3 @@
 ((): void => {
-    var app = angular.module("app", [
-        "720kb.socialshare"
-    ]);
+
 });
