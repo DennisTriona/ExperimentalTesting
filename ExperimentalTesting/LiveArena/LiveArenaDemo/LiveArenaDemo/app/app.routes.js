a491398b7be99765574f7d90f60aa2013b07cec4
INTRATRIONA\FreSan <Fredrik.Sandstrom@triona.se>
2016-09-16 10:56:57 +0200

    LiveArenaShareDemo: U: Added CSS and resources

--- ExperimentalTesting/LiveArena/LiveArenaDemo/LiveArenaDemo/app/app.routes.js
+++ ExperimentalTesting/LiveArena/LiveArenaDemo/LiveArenaDemo/app/app.routes.js
@@ -1,1 +1,14 @@
+var app;
+(function (app) {
+    var Routes = (function () {
+        function Routes() {
+        }
+        Routes.configureRoutes = function ($routeProvider) {
+        };
+        // Class that manages all the routing
+        Routes.$inject = ["$routeProvider"];
+        return Routes;
+    }());
+    app.Routes = Routes;
+})(app || (app = {}));
 //# sourceMappingURL=app.routes.js.map
